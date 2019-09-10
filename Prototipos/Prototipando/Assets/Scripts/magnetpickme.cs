fac3bb175a254923a75e9a4cac694ddb839b4327
Agus <amoure@gmail.com>
2019-09-10 22:34:37 +0200

    Creado el prototipo de la mecanica del iman

---
+++ Prototipos/Prototipando/Assets/Scripts/magnetpickme.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class magnetpickme : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.GetChild(0).transform.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Space))
+        transform.GetChild(0).transform.gameObject.SetActive(true);
+    }
+}
