9f6db9e084eedbbd71404ad9d27cbfcc6898d905
Agus <amoure@gmail.com>
2019-09-10 20:41:14 +0200

    Creado el protipo del jetpack

---
+++ Prototipos/Prototipando/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float speedX;
+    Rigidbody2D rigidbody;
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float x = Input.GetAxis("Horizontal");
+        rigidbody.velocity = new Vector2(speedX * x, rigidbody.velocity.y);
+	}
+}
