5b7ff1c238052606bd09d739f4a37c47d95cf01e
Agusmoure <amoure@ucm.es>
2019-10-16 12:20:22 +0200

    añadidos metodos en la clase player

--- EmpezandodeCero/phasertemplate-master/player.js
+++ EmpezandodeCero/phasertemplate-master/player.js
@@ -4,6 +4,10 @@ constructor(scene){
 
 let x=0;
 let y=0;
+let aire=false;
+let speedX=160;
+let speedY =50;
+let vehicle=false;
 super(scene,x,y,'Player');
 
 
@@ -14,7 +18,24 @@ super(scene,x,y,'Player');
 
 changeG(){
     this.body.gravity.y();
-
-
 }
+aumentSpeed(){
+    speedX *=4;
+}
+resetSpeed(){
+    speedX=160;
+}
+getSpeedX(){
+    return speedX;
+}
+getSpeedY(){
+    return speedY;
+}
+setVehicle(haveVehicle){
+    vehicle=haveVehicle;
+}
+getVehicle(){
+    return vehicle;
+}
+
 }
