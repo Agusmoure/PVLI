65b85778068483d925c41e2629aab021bffd39b8
czepieldavid <dczepiel@ucm.es>
2019-10-24 12:10:14 +0200

    Haciendo limpieza
    Ambos miembros del grupo han estado presentes

--- SaveyourSon/src/game.js
+++ SaveyourSon/src/game.js
@@ -8,12 +8,12 @@ export default class Game extends Phaser.Scene {
     super({ key: 'main' });
   }
   preload() {
-    this.load.image('sky', 'assets/sky.png');
-    this.load.image('ground', 'assets/platform.png');
+    this.load.image('sky', '../assets/sky.png');
+    this.load.image('ground', '../assets/platform.png');
 
-    this.load.image('star', 'assets/star.png');
-    this.load.image('bomb', 'assets/bomb.png');
-    this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+    this.load.image('star', '../assets/star.png');
+    this.load.image('bomb', '../assets/bomb.png');
+    this.load.spritesheet('dude', '../assets/dude.png', { frameWidth: 32, frameHeight: 48 });
   //  this.load.tilemapTiledJSON('level1Tilemap', 'level1.json');
    // this.load.image('patronesTilemap', 'assets/patrones.png');
     
