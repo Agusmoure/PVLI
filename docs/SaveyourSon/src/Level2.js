7bba1aeadedccea024c4dc9fa106602d389c677a
czepieldavid <dczepiel@ucm.es>
2019-12-19 11:58:42 +0100

    Solucionado error pistola gancho

--- docs/SaveyourSon/src/Level2.js
+++ docs/SaveyourSon/src/Level2.js
@@ -163,7 +163,7 @@ this.HookGunProyectiles.add(this.HookGunProyectile);
     this.HookGun.destroy();
     this.HookGun = new HookGun(this,this.lvM,19600,1350).setScale(0.2);
     this.physics.add.overlap(this.player,this.HookGun,this.player.changeModifierGancho,null,this.player);
-    this.physics.add.overlap(this.player,this.HookGun,this.HookGun.changeModifier,null,this.HookGun);
+    this.physics.add.overlap(this.player,this.HookGun,this.HookGun.PickGun,null,this.HookGun);
     this.physics.add.overlap(this.player,this.HookGun,this.noBombaEnMano,null,this);
     this.physics.add.collider(this.HookGun,this.background);
 
